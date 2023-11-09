a9feeb949d11e9a8f15eb6920d21d51d154965df
jgordini <jgordini@users.noreply.github.com>
2023-11-09 17:00:56 +0000

    deploy: 2529af1311c8236c1fc9ecb6e595269e60ad2215

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,10 @@ import withSearch from './src/markdoc/search.mjs'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
+  assetPrefix: process.env.BASE_PATH || "", // Add this line for GitHub Pages
 }
 
 export default withSearch(
   withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig),
 )
+
