3be763c4b3eab8c5910d1183d06ec9a0b9ebcb40
jgordini <jgordini@uab.edu>
2023-11-09 11:00:22 -0600

    update

--- src/components/DocsHeader.tsx
+++ src/components/DocsHeader.tsx
@@ -3,29 +3,35 @@
 import { usePathname } from 'next/navigation'
 
 import { navigation } from '@/lib/navigation'
+import Head from 'next/head';
 
 export function DocsHeader({ title }: { title?: string }) {
-  let pathname = usePathname()
+  let pathname = usePathname();
   let section = navigation.find((section) =>
     section.links.find((link) => link.href === pathname),
-  )
+  );
 
   if (!title && !section) {
-    return null
+    return null;
   }
 
   return (
-    <header className="mb-9 space-y-1">
-      {section && (
-        <p className="font-display text-sm font-medium text-sky-500">
-          {section.title}
-        </p>
-      )}
-      {title && (
-        <h1 className="font-display text-3xl tracking-tight text-slate-900 dark:text-white">
-          {title}
-        </h1>
-      )}
-    </header>
-  )
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
+      <header className="mb-9 space-y-1">
+        {section && (
+          <p className="font-display text-sm font-medium text-sky-500">
+            {section.title}
+          </p>
+        )}
+        {title && (
+          <h1 className="font-display text-3xl tracking-tight text-slate-900 dark:text-white">
+            {title}
+          </h1>
+        )}
+      </header>
+    </>
+  );
 }
